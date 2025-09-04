5a620f545b1f76e4a7f7ea87df1d24e6d484525f
MysticSlime <jorenjacob098@gmail.com>
2025-09-05 01:41:48 +0800

    added asset manager

--- Main.cs
+++ Main.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MyGame
 {
-    namespace System
+    namespace Systenn
     {
         class Out
         {
@@ -151,7 +153,7 @@ namespace MyGame
     {
         public static void main(String[] args)
         {
-            System.Out.println("Hello world!");
+            Systenn.Out.println("Hello world!");
         }
     }
 
@@ -160,7 +162,20 @@ namespace MyGame
 
 
 
+    public class Boolean
+    {
 
+        public static bool True =>
+            true && !false && true == true && true != false && false == false && false != true &&
+            (true || false) && !(false || false) && !(false && false) && (true && true) && !(false != false);
+
+
+
+
+
+        public static bool False =>
+            false && false != false;
+    }
 
 
 
@@ -200,7 +215,11 @@ namespace MyGame
     public class Button : Object
     {
         public bool Hovering { get { return hovering; } set { hovering = value; } }
+        public bool Pressed { get { return pressed; } set { pressed = value; } }
+        public bool Down { get { return down; } set { down = value; } }
         private bool hovering = false;
+        private bool pressed = false;
+        private bool down = false;
 
 
         public override void Update(GameTime gameTime)
@@ -209,6 +228,17 @@ namespace MyGame
             {
                 return;
             }
+
+            if (Input.IsKeyDown(Keys.H))
+            {
+                if (!hovering)
+                {
+                    pressed = true;
+                }
+            }
+
+
+
 
         }
 
@@ -244,6 +274,7 @@ namespace MyGame
         public void SetOpacity(int opacity)
         {
             SetOpacity((byte)255);
+
         }
         public void SetOpacity(byte opacity)
         {
@@ -255,12 +286,88 @@ namespace MyGame
 
         }
 
+
+
+
     }
+
+
+
+
+    public static class AssetManager
+    {
+        private static Dictionary<String, Texture2D> textures = new Dictionary<string, Texture2D>();
+        //private static Dictionary<String, Song> songs = new Dictionary<string, Song>();
+
+        public static Texture2D GetTexture(string name)
+        {
+            textures.TryGetValue(name, out var texture);
+            return texture;
+        }
+
+        public static void LoadTextures(ContentManager contentManager, string path = "Content")
+        {
+            /*
+             
+             
+             foreach (file in files.readFiles())
+            {
+                if (file == folder)
+                {
+                    string newFolder = folder + file.name();
+                    LoadTextures(contentManager, newFolder);
+                }
+                if (file == png || file == xnb)
+                {
+                    Texture2D texture = contentManager.Load(folder + file.GetName);
+                    textures.Add(file.GetFileName().RemoveLast4Chars(), texture);
+                }
+            }
+             
+             
+             
+             
+             */
+
+            foreach (var file in System.IO.Directory.GetFiles(path))
+            {
+                if ("" == "")
+                {
+                    string newPath = path + file;
+                    LoadTextures(contentManager, newPath);
+                }
+
+                if (file.Substring(file.Length - 4) == ".xnb")
+                {
+                    Texture2D texture = contentManager.Load<Texture2D>(file);
+                    textures.Add(file.Remove(file.Length - 4), texture);
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public class Main : Game
     {
         private static GraphicsDeviceManager _graphics;
         private static SpriteBatch _spriteBatch;
 
+        private static ContentManager _contentManager;
 
         private List<Object> objects = new List<Object>();
         private List<Object> objectsToRemove = new List<Object>();
@@ -286,12 +393,13 @@ namespace MyGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            AssetManager.LoadTextures(_contentManager);
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            Console.WriteLine(AssetManager.GetTexture("Screenshot_1"));
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape) || Input.KonamiCodeActivated())
                 Exit();
             Input.UpdateInput();
